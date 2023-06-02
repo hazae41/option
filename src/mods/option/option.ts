09197fd72135282503328dbfd2674c8d8531e432
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-06-02 21:45:05 +0200

    mapSync

--- src/mods/option/option.ts
+++ src/mods/option/option.ts
@@ -1,5 +1,10 @@
+import { Promiseable } from "libs/promises/promises.js";
 import { None } from "./none.js";
 import { Some } from "./some.js";
+
+export type Optional<T> =
+  | T
+  | undefined
 
 export type Option<T> =
   | Some<T>
@@ -12,11 +17,19 @@ export namespace Option {
    * @param inner 
    * @returns `Some<T>` if `T`, `None` if `undefined`
    */
-  export function from<T>(inner: T | undefined): Option<T> {
+  export function from<T>(inner: Optional<T>): Option<T> {
     if (inner === undefined)
       return new None()
     else
       return new Some(inner)
+  }
+
+  export async function map<T, U>(inner: Optional<T>, mapper: (inner: T) => Promiseable<U>) {
+    return Option.from(inner).map(mapper).then(o => o.get())
+  }
+
+  export function mapSync<T, U>(inner: Optional<T>, mapper: (inner: T) => U) {
+    return Option.from(inner).mapSync(mapper).get()
   }
 
 }
