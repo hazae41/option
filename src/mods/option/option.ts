b4a5c1591828234f4172eb80ab833bed80271631
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-08-29 19:07:34 +0200

    nullable

--- src/mods/option/option.ts
+++ src/mods/option/option.ts
@@ -2,8 +2,14 @@ import { Promiseable } from "libs/promises/promises.js";
 import { None, NoneInit } from "./none.js";
 import { Some, SomeInit } from "./some.js";
 
-export type Optional<T> =
+export type Nullable<T> =
   T | undefined | null
+
+export type Optional<T> =
+  T | undefined
+
+export type NonOptional<T> =
+  Exclude<T, undefined>
 
 export type Option<T> =
   | Some<T>
@@ -22,25 +28,25 @@ export namespace Option {
   }
 
   /**
-   * Create an Option from a maybe undefined value
+   * Create an Option from a nullable value
    * @param inner 
    * @returns `Some<T>` if `T`, `None` if `undefined`
    */
-  export function wrap<T>(inner: Optional<T>): Option<T> {
+  export function wrap<T>(inner: Nullable<T>): Option<T> {
     if (inner == null)
       return new None()
     return new Some(inner)
   }
 
-  export async function map<T, U>(inner: Optional<T>, mapper: (inner: T) => Promiseable<U>) {
+  export async function map<T, U>(inner: Nullable<T>, mapper: (inner: T) => Promiseable<U>) {
     return Option.wrap(inner).map(mapper).then(o => o.get())
   }
 
-  export function mapSync<T, U>(inner: Optional<T>, mapper: (inner: T) => U) {
+  export function mapSync<T, U>(inner: Nullable<T>, mapper: (inner: T) => U) {
     return Option.wrap(inner).mapSync(mapper).get()
   }
 
-  export function unwrap<T>(inner: Optional<T>) {
+  export function unwrap<T>(inner: Nullable<T>) {
     return Option.wrap(inner).unwrap()
   }
 
